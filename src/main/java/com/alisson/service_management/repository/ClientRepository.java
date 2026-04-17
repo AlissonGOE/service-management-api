335c7eb7983db2b5a873014a151fac50f0b3b9c5
Alisson Evangelista <alissongiovane22@gmail.com>
2026-04-17 00:20:53 -0300

    feat(repository): add findByName method to ClientRepository

--- src/main/java/com/alisson/service_management/repository/ClientRepository.java
+++ src/main/java/com/alisson/service_management/repository/ClientRepository.java
@@ -3,5 +3,8 @@ package com.alisson.service_management.repository;
 import com.alisson.service_management.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ClientRepository extends JpaRepository<Client, Long> {
+    Optional<Client> findByName(String name);
 }
