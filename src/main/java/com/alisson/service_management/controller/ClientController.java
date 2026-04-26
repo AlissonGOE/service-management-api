b6685333010207886460e871ee92aab6be3b028b
Alisson Evangelista <alissongiovane22@gmail.com>
2026-04-26 15:22:47 -0300

    feat(client): implement CRUD endpoints in controller

--- src/main/java/com/alisson/service_management/controller/ClientController.java
+++ src/main/java/com/alisson/service_management/controller/ClientController.java
@@ -3,14 +3,14 @@ package com.alisson.service_management.controller;
 import com.alisson.service_management.dto.ClientDTO;
 import com.alisson.service_management.service.ClientService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("clients")
@@ -33,6 +33,94 @@ public class ClientController {
         ClientDTO newClient = clientService.createClient(client);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body("Client created successfully " + newClient.getName() + " (Id): " + newClient.getId());
+    }
+
+    // Read
+    @GetMapping("/list")
+    @Operation(summary = "List all clients", description = "Route to list all clients")
+    public ResponseEntity<List<ClientDTO>> findAll() {
+        List<ClientDTO> clients = clientService.findAll();
+        return ResponseEntity.ok(clients);
+    }
+
+    // Read by Id
+    @GetMapping("/list/id/{id}")
+    @Operation(summary = "List the client by (Id)", description = "Route to list the client by (Id).")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Client found successfully."),
+            @ApiResponse(responseCode = "404", description = "Client was not found.")
+    })
+    public ResponseEntity<?> findById(
+            @Parameter(description = "The user enters the (Id) in the request body.")
+            @PathVariable Long id) {
+        ClientDTO client = clientService.findById(id);
+        if (client != null) {
+            return ResponseEntity.ok(client);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("The client with (Id): " + id + " was not found.");
+        }
+
+    }
+
+    // Read by Name
+    @GetMapping("/list/name/{name}")
+    @Operation(summary = "List the client by (Name)", description = "Route to list the client by (Name).")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Client found successfully."),
+            @ApiResponse(responseCode = "404", description = "Client was not found.")
+    })
+    public ResponseEntity<?> findByName(
+            @Parameter(description = "The user enters the (Name) in the request body.")
+            @PathVariable String name) {
+        ClientDTO client = clientService.findByName(name);
+        if (client != null) {
+            return ResponseEntity.ok(client);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("The client with (Name): " + name + " was not found.");
+        }
+
+    }
+
+    // Update by Id
+    @PutMapping("/update/{id}")
+    @Operation(summary = "Update the client by (Id)", description = "Route to update the client by (Id).")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Client updated successfully"),
+            @ApiResponse(responseCode = "404", description = "Client was not found.")
+    })
+    public ResponseEntity<?> updateById(
+            @Parameter(description = "The user enters the (Id) in the request body.")
+            @PathVariable Long id,
+            @Parameter(description = "The user enters the (Data) in the request body.")
+            @PathVariable ClientDTO updatedClient) {
+        ClientDTO client = clientService.updateById(id, updatedClient);
+        if (client != null) {
+            return ResponseEntity.ok(client);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("The client with (Id): "  + id +  " was not found.");
+        }
+    }
+
+    // Delete by Id
+    @DeleteMapping("/delet/{id}")
+    @Operation(summary = "Delete the client by (Id)", description = "Route to delete the client by (Id).")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Client deleted successfully."),
+            @ApiResponse(responseCode = "404", description = "Client was not found.")
+    })
+    public  ResponseEntity<String> deletById(
+            @Parameter(description = "The user enters the (Id) in the request body.")
+            @PathVariable Long id) {
+        if (clientService.findById(id) != null) {
+            clientService.deleteClientPerId(id);
+            return ResponseEntity.ok("The client with (Id): " + id + " was deletede successfully.");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("The client with (Id): "  + id +  " was not found.");
+        }
     }
 
 }
