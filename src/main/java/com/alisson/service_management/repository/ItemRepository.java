9af833a9161232ee57f44d4482362251a593e6de
Alisson Evangelista <alissongiovane22@gmail.com>
2026-05-03 15:29:20 -0300

    feat(item-repository): add findByServiceId and findByServiceClientId query methods

--- src/main/java/com/alisson/service_management/repository/ItemRepository.java
+++ src/main/java/com/alisson/service_management/repository/ItemRepository.java
@@ -1,4 +1,11 @@
 package com.alisson.service_management.repository;
 
-public class ItemRepository {
+import com.alisson.service_management.model.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
+    List<Item> findByServiceId(Long serviceId);
+    List<Item> findByServiceClientId(Long clientId);
 }
